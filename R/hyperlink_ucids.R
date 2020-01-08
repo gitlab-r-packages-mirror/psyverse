3836caf7abb486d79a3be79731c905aaa66bc369
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2020-01-08 12:36:27 +0100

    Fixed Rmd hyperlink

--- R/hyperlink_ucids.R
+++ R/hyperlink_ucids.R
@@ -1,6 +1,6 @@
 hyperlink_ucids <- function(x,
                             regex = "dct:([a-zA-Z0-9_]+)",
-                            replacement = "(dct:\\1)[#\\1]") {
+                            replacement = "[dct:\\1](#\\1)") {
   return(gsub(regex,
               replacement,
               x));
