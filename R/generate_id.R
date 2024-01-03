60eb6ddf2ec4b973dfea72381e84895a66d17b52
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2024-01-03 19:52:29 +0100

    tweaked id generation

--- R/generate_id.R
+++ R/generate_id.R
@@ -12,6 +12,7 @@
 #' @param stopOnIllegalChars Whether to [base::stop()] or produce a [base::warning()]
 #'        when encountering illegal characters (i.e. anything other than a letter or
 #'        underscore).
+#' @param origin The time and date to use to generate the identifier.
 #'
 #' @return a character vector containing the identifier(s).
 #' @examples generate_id('attitude');
@@ -19,7 +20,8 @@
 #' @rdname generate_id
 #' @export
 generate_id <- function(prefix = paste(sample(letters, 4), collapse=""),
-                        stopOnIllegalChars = FALSE) {
+                        stopOnIllegalChars = FALSE,
+                        origin = Sys.time()) {
 
   if (length(prefix) > 1) {
     warning("Use `generate_ids` to generate multiple ids at once; ",
@@ -28,7 +30,7 @@ generate_id <- function(prefix = paste(sample(letters, 4), collapse=""),
                         stopOnIllegalChars=stopOnIllegalChars));
   }
 
-  if ((grepl("[^a-zA-Z_]+",
+  if ((!grepl("^[a-zA-Z_][a-zA-Z0-9_]*",
              prefix))) {
     if (stopOnIllegalChars) {
       stop("The specified prefix contains illegal characters, and argument ",
@@ -36,15 +38,19 @@ generate_id <- function(prefix = paste(sample(letters, 4), collapse=""),
     } else {
       warning("The specified prefix contains illegal characters, and argument ",
               "`stopOnIllegalChars` is set to FALSE, so I'm removing them.");
+      while (grepl("^[^a-zA-Z_]", prefix)) {
+        ### First character is illegal
+        prefix <- substring(prefix, 2);
+      }
       prefix <-
-        gsub("[^a-zA-Z_]+",
+        gsub("[^a-zA-Z0-9_]+",
              "",
              prefix);
     }
   };
 
   #timeNrString <- gsub('\\.', '', format(Sys.time(), "%y%m%d%H%M%OS2"));
-  timeNrString <- as.character(round(as.numeric(Sys.time()) * 100, 0));
+  timeNrString <- as.character(round(as.numeric(origin) * 100, 0));
 
   res <- numericToBase30(as.numeric(timeNrString));
   return(paste0(prefix, "_", res));
