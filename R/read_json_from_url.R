1ad0709b3a8fbfadddc9f19ce02fb0df8310bb12
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2024-01-21 19:40:43 +0100

    dus

--- R/read_json_from_url.R
+++ R/read_json_from_url.R
@@ -1,8 +1,23 @@
-
+#' Read an object from JSON embedded in a HTML page
+#'
+#' You normally don't use this; this function is exposed for convenience,
+#' but you normally use functions that import specific things (e.g.
+#' questionnaires).
+#'
+#' @param url The URL where the JSON can be found.
+#' @param attribute The attribute holding the JSON.
+#' @param elementId,elementClass Specify exactly one of these: either the
+#' identifier of the element holding the JSON, or the class name of a class
+#' that may be applied to several elements (which will then all be returned).
+#'
+#' @return An object if specifying `elementId`, or a list of objects if
+#' specifying `elementClass` (although is there's only one element of
+#' `elementClass`, the only object will returned anyway).
+#' @export
 read_json_from_url <- function(url,
+                               attribute,
                                elementId = NULL,
-                               elementClass = NULL,
-                               attribute) {
+                               elementClass = NULL) {
 
   html <- xml2::read_html(x = url);
 
@@ -55,16 +70,16 @@ read_json_from_url <- function(url,
 }
 
 
-test1 <-
-read_json_from_url("https://operationalizations.com/questionnaires/json",
-                   elementId="operationalizations-com-data-bfi44eng_7rrlcsyp",
-                   attribute = "data-operationalizations-com")
-
-test2 <-
-
-  read_json_from_url("https://operationalizations.com/questionnaires/json",
-                   elementClass = "operationalizations-com-json",
-                   attribute = "data-operationalizations-com")
-
-identical(test1, test2[[1]]);
+ # test1 <-
+# read_json_from_url("https://operationalizations.com/questionnaires/json",
+#                    elementId="operationalizations-com-data-bfi44eng_7rrlcsyp",
+#                    attribute = "data-operationalizations-com")
+#
+# test2 <-
+#
+#   read_json_from_url("https://operationalizations.com/questionnaires/json",
+#                    elementClass = "operationalizations-com-json",
+#                    attribute = "data-operationalizations-com")
+#
+# identical(test1, test2[[1]]);
 
