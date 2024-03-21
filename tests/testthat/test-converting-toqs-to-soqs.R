9151393a63b2bfa9363e7f81a45da02b02d6d82d
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2024-03-21 19:23:12 +0100

    added BFI-10 to test file

--- tests/testthat/test-converting-toqs-to-soqs.R
+++ tests/testthat/test-converting-toqs-to-soqs.R
@@ -27,5 +27,26 @@ testthat::test_that("A TOQ spec can be converted to a SOQ spec", {
 
   testthat::expect_equal(file.exists(tmpFile), TRUE);
 
+  tmpFile <- tempfile(pattern = "bfi10eng_7sp9mjx3_",
+                      fileext = ".yml");
+
+  soq <-
+    psyverse::toq_to_soq(
+      paste0(
+        "https://docs.google.com/spreadsheets/d/",
+        "1atGTOs9RGTISDxiSH0IfSH5fDPYaDhnJEdbXqWqDRto"
+      ),
+      returnYAML = TRUE
+    );
+
+  writeLines(
+    soq,
+    tmpFile
+  );
+
+  cat(tmpFile);
+
+  testthat::expect_equal(file.exists(tmpFile), TRUE);
+
 });
 
